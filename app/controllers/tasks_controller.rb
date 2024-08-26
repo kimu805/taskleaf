3b66bca83f9251aa101ed81353aac47b4d9fb5ed
kimu805 <kimukimuok805@gmail.com>
2024-08-26 12:10:22 +0900

    add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,29 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit
   end
 
   def show
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
