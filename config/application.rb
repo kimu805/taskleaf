5781758269b196bf9e88e701d6e7ab8d1b68c30b
kimu805 <kimukimuok805@gmail.com>
2024-08-26 12:18:55 +0900

    change time to japanese

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Taskleaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # config.generators do |g|
     #   g.helper          false
     #   g.test_framework  false
