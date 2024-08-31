41b2912b4c692799a1d18c42e506b546a55d1373
kimu805 <kimukimuok805@gmail.com>
2024-08-31 16:47:38 +0900

    implement search

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,4 +12,8 @@ class Task < ApplicationRecord
   def validate_name_not_including_comma
     errors.add(:name, "にカンマを含めることができません。") if name&.include?(",")
   end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "name", "description"]
+  end
 end
