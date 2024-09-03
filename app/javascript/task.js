5c310e309eed3c28a72d4536443b7438bd0a89ed
kimu805 <kimukimuok805@gmail.com>
2024-09-03 16:55:23 +0900

    delete console.log

--- app/javascript/task.js
+++ app/javascript/task.js
@@ -1,7 +1,6 @@
 const destroy = () => {
   document.querySelectorAll(".delete").forEach(function(a) {
     a.addEventListener("click", function() {
-      console.log("読み込み完了")
       var frameTag = a.parentNode
       var td = frameTag.parentNode
       var tr = td.parentNode
