cbcd7f5b77752d8dfc1825683bc9d6f42e8b6ffd
kimu805 <kimukimuok805@gmail.com>
2024-09-03 14:19:06 +0900

    add destroy action to task.js

--- app/javascript/task.js
+++ app/javascript/task.js
@@ -1,2 +1,11 @@
+const destroy = () => {
+  document.querySelectorAll(".delete").forEach(function(a) {
+    a.addEventListener("click", function() {
+      var td = a.parentNode
+      var tr = td.parentNode
+      tr.style.display = "none"
+    })
+  })
+}
 
-
+window.addEventListener("turbo:load", destroy)
