06699a404cf3da07e7d1ff55b38ea5b32fbb6d52
kimu805 <kimukimuok805@gmail.com>
2024-09-04 14:31:57 +0900

    fin preview

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,29 @@
 function preview() {
-  console.log("読み込み完了")
+  const previews = document.getElementById("previews")
+  const taskImage = document.getElementById("task_image")
+
+  if (!previews) return null
+
+  taskImage.addEventListener("change", (e) => {
+
+    const alreadyPreview = document.querySelector(".preview")
+    if (alreadyPreview) alreadyPreview.remove()
+
+    const file = e.target.files[0]
+    const blob = URL.createObjectURL(file)
+
+    const preview = document.createElement("div")
+    preview.setAttribute("class", "preview mt-3")
+
+    const previewImage = document.createElement("img")
+    previewImage.setAttribute("src", blob)
+    previewImage.setAttribute("alt", "画像プレビュー")
+    previewImage.setAttribute("width", "300")
+    previewImage.setAttribute("height", "200")
+
+    preview.appendChild(previewImage)
+    previews.appendChild(preview)
+  })
 }
 
 window.addEventListener("turbo:load", preview)
